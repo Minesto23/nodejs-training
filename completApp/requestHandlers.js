c68fa97ad2c37acbda5af4776b5f476e97576b47
Miguel Morales <minesto23@gmail.com>
2021-02-12 12:08:46 -0400

    second commit

--- completApp/requestHandlers.js
+++ completApp/requestHandlers.js
@@ -1,19 +1,19 @@
-function start(){
-	console.log("Manipulador de peticion start ha sido llamado.");
-	
-	function sleep(milliSeconds) {
-		// obten la hora actual
-		var startTime = new Date().getTime();
-		// atasca la cpu
-		while (new Date().getTime() < startTime + milliSeconds);
-	}
-	sleep(10000);	
-	return "Hello start";
+var exec = require("child_process").exec;
+
+function start(response){
+	console.log("Manipulador de peticion start ha sido llamado.");	
+	exec("ls -lah", function (error, stdout, stderr) {
+		response.writeHead(200, {"Content-Type":"text/html"});
+		response.write(stdout);
+		response.end();
+	})
 }
 
-function upload(){
+function upload(response){
 	console.log("Manipulador de peticion upload ha sido llamado.");
-	return "Hello upload";
+	response.writeHead(200, {"Content-Type":"text/html"});
+	response.write("Hello Upload");
+	response.end();
 }
 
 exports.start = start;
